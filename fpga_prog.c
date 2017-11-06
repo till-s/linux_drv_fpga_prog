cdddc7b3972dbc32c0e2a6b0271fcdef23f806e3
till straumann <till.straumann@alumni.tu-berlin.de>
2017-11-06 12:53:15 -0800

    Added slac licensing

--- fpga_prog.c
+++ fpga_prog.c
@@ -1,3 +1,15 @@
+/* Copyright Notice
+ * ================
+ * This file is part of the fpga_prog linux kernel module.
+ * It is subject to the license terms in the LICENSE.txt
+ * file found in the top-level directory of this distribution and at
+ * https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ *
+ * No part of the software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in
+ * the LICENSE.txt file.
+ */
+
 /*
  * 'Glue' driver to use FPGA-manager without device-tree overlays etc.
  *
